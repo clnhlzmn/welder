4aa93461bd9bc6851d4d5b00ec8e63100c437a55
Colin Holzman <colin.holzman@protonmail.com>
2021-12-31 23:45:14 -0800

    added diy igbt model to filter sim

--- simulation/pwl.py
+++ simulation/pwl.py
@@ -1,8 +1,8 @@
-i = 0.0000001
+i = 0.000001
 t_max = 0.02
 i_max = int(t_max / i)
 
-f_sw = 100000
+f_sw = 20000
 p_sw = int((1 / f_sw) / i)
 f_ac = 100
 p_ac = int((1 / f_ac) / i)
@@ -12,11 +12,14 @@ v_out = 15
 v_in = 48
 sw_duty = v_out / v_in
 sw_on = int(p_sw * sw_duty)
+v_gs = 15
 
 sw_state = 0
 ac_state = 0
 
-with open('pwl.txt', 'w+') as out:
+with (open('pwl.txt', 'w+') as out,
+    open('outp.txt', 'w+') as outp,
+    open('outn.txt', 'w+') as outn):
     on_time = 0
     for t in range(i_max):
         # Count duration that switch is on
@@ -40,3 +43,6 @@ with open('pwl.txt', 'w+') as out:
         output = 48 if sw_state else 0
         output = output if ac_state else -output
         out.write("{} {}\n".format(t * i, output))
+        
+        outp.write("{} {}\n".format(t * i, v_gs if ac_state and sw_state else 0))
+        outn.write("{} {}\n".format(t * i, v_gs if not ac_state and sw_state else 0))
